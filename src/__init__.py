a1323496d90c3a60da9c60d83cf007e730f7a8e1
DivyaVarshiniSuravaram <divyavarshini.suravaram@sjsu.edu>
2024-11-22 15:45:36 -0800

    Average occupancy heat maps and weekly occupancy trends done

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.pre_processing import get_train_test_data
 from src.model import train_model
-from src.plots import daily_average_occupancy, hourly_average_occupancy
+from src.plots import daily_average_occupancy, hourly_average_occupancy, average_occupancy_heatmap, weekly_occupancy_trends
 
 
 def chain_of_execution():
@@ -13,3 +13,5 @@ def visualization():
     train_data, test_data, train_labels, test_labels = get_train_test_data()
     daily_average_occupancy(train_data)
     hourly_average_occupancy(train_data)
+    average_occupancy_heatmap(train_data)
+    weekly_occupancy_trends(train_data)
